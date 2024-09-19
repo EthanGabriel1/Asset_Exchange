f24870529a09171cfbade68796ecdf65adce6a2e
Itsan <etto.rolloque.swu@phinmaed.com>
2024-09-20 02:23:07 +0800

    Initial Commit

--- app/src/main/java/com/example/assetexchange/MyBottomSheetFragment.java
+++ app/src/main/java/com/example/assetexchange/MyBottomSheetFragment.java
@@ -4,17 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import androidx.cardview.widget.CardView;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class MyBottomSheetFragment extends BottomSheetDialogFragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.bottom_sheet_layout, container, false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
+
+        // Find the addedlist_container in the layout
+        CardView addedListContainer = view.findViewById(R.id.Added_lists_container);
+
+        // Set click listener to show the BottomSheet_AddedLists
+        addedListContainer.setOnClickListener(v -> {
+            BottomSheet_AddedLists bottomSheetAddedLists = new BottomSheet_AddedLists();
+            bottomSheetAddedLists.show(getParentFragmentManager(), bottomSheetAddedLists.getTag());
+        });
+
+        return view;
     }
-
-
 }
