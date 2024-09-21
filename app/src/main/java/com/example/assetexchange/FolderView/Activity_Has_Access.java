ab66887be7dac06d7523b1b17f30f778f351e5d2
Itsan <etto.rolloque.swu@phinmaed.com>
2024-09-21 23:26:48 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/assetexchange/FolderView/Activity_Has_Access.java
@@ -0,0 +1,18 @@
+package com.example.assetexchange.FolderView;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assetexchange.R;
+
+public class Activity_Has_Access extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_has_access);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/assetexchange/FolderView/Revisions_Page.java
@@ -0,0 +1,18 @@
+package com.example.assetexchange.FolderView;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assetexchange.R;
+
+public class Revisions_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.revisions_page);
+
+
+    }
+}
